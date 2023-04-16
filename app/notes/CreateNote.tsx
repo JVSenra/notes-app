2cb4c5e45da7b3fc99923778a462952880200e5a
João Victor Senra <joaovictor.senra@estudante.ufjf.br>
2023-04-16 02:09:03 -0300

    Adiciona CreateNote

---
+++ app/notes/CreateNote.tsx
@@ -0,0 +1,9 @@
+'use client'; //Ativa a renderização do componentes no cliente
+
+export default function CreateNote() {
+  return (
+		<div>
+      <h1>Create Note</h1>
+    </div>
+  );
+}
--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -1,3 +1,4 @@
+'use client'; //Ativa a renderização do componentes no cliente
 export default function Error() {
-    return <p>error...</p>
-  }
+  return <p>error...</p>
+}
